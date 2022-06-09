70690e5b4c1a96e703dc667499c66a961950771b
software-wanderer <software-wanderer@no.reply.github.com>
2022-06-09 14:31:14 +0900

    edit: Add sensor pin number in Sensor Enum

--- shiny-carnival/src/sensor.h
+++ shiny-carnival/src/sensor.h
@@ -1,13 +1,12 @@
 #include "camera.h"
 
-// TODO: 4, 5, 6번 센서 몇번 핀인지 기입
 enum Sensor{
     SENSOR_1 = 6,
     SENSOR_2 = 23,
     SENSOR_3 = 25,
-    SENSOR_4 = 0,
-    SENSOR_5 = 0,
-    SENSOR_6 = 0
+    SENSOR_4 = 25,
+    SENSOR_5 = 23,
+    SENSOR_6 = 6
 };
 
 enum Mode{
